65b3b435c34638a8eb7cba11e2cdd285ea6cfa4c
Jonas Dawid <jonasdawinho@gmail.com>
2026-05-13 11:23:48 -0300

    exercicios

---
+++ LOGICA_PROGRAMAÇAO/PROJETOS/cancelas_shopping.py
@@ -0,0 +1,58 @@
+# Criar um algoritmo para que:
+
+# Projeto 1:
+# Projeto: Precisamos de um algoritmo para gerenciamento de cancelas para um shopping.
+# Toda entrada e saída irá ser sinalizada
+# Valores para entrada e permanência do veículo deverá ser pergutado
+# As entrada deverão ser registradas por placa.
+#
+# Passo 1:  
+# Perguntar informações sobre o veiculo ou forma acesso
+# Pressionar o botao para emitir ticket
+# Verificar se possui TAG para acesso liberado
+# Se possuir erros informar ao usuário
+
+# Passo 2:
+# Verificar tempo de permanência
+# Valor a ser cobrado
+
+# Passo 3:
+# Saída como será?
+# Calcular tempo de permanência
+# Se for TAG gerar na fatura da TAG
+# Pagar ticket
+# Devolver ticket na saída
+
+# Passo 4:
+# Gerar relatório de entradas e saídas
+# Tratamento de Erros
+# Revisão do código
+
+# print (" olá bem vindo ao shopping ")
+# print1 = (" valor do estacioanmento 20,00 por hora ")
+
+# passo2 =print( input("isira a placa do seu veículo: "))
+# passo3 =print ( input(" pretende ficar quanto tempo ?:"))
+# passo4 = (passo3 * 20)
+# print (" valor a pagar ", passo4)
+# print(" selecione a forma de pagamento ")
+# print (" vículo identificado")
+# print(" gerando ticket ")
+# print (" boa visita ")
+
+
+print(" olá bem vindo ao shopping ")
+print(" valor do estacionamento 20,00 por hora ")
+passo2 = input("insira a placa do seu veículo: ")
+passo3 = int(input(" pretende ficar quanto tempo ? (digite o tempo em horas): "))
+passo4 = passo3 * 20
+print(" valor a pagar: R$", passo4)
+print(" selecione a forma de pagamento ")
+print(" veículo identificado")
+print(" gerando ticket ")
+print(" boa visita ")
+ # Saída 
+
+print ("olá novamente ")
+print ("apresente o ticket obtido no período de entrada ")
+print (" obrigado, volte sempre ")
